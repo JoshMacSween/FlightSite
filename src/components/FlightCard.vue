4710e9e2982e48594d83f067adf580ef74acc6cd
Joshua MacSween <joshmacsween@gmail.com>
2020-06-23 22:20:17 -0600

    button

--- src/components/FlightCard.vue
+++ src/components/FlightCard.vue
@@ -3,6 +3,7 @@
     <div class="card">
       <h2>{{ title }}</h2>
       <p>Length: {{ length }} hours</p>
+      <button class="bookButton">Book Flight</button>
     </div>
   </div>
 </template>
@@ -18,7 +19,19 @@
   margin: auto;
   width: 50%;
   border: 3px solid green;
+  border-radius: 10px;
   padding: 10px;
   margin-bottom: 10px;
 }
+  .bookButton {
+    background-color: aliceblue;
+    padding: 15px 32px;
+    display: inline-block;
+    border-radius: 4px;
+  }
+  .bookButton:hover {
+    padding: 20px 37px;
+    transition-duration: 0.4s;
+
+  }
 </style>
